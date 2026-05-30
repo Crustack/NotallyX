b8e8115037673265b4bae754b32eb8f214e77d9c
Crustack <crustackk@gmail.com>
2026-05-30 18:51:20 +0200

    Fix PR issues

--- app/src/main/java/com/philkes/notallyx/data/model/ConverterErrorReporter.kt
+++ app/src/main/java/com/philkes/notallyx/data/model/ConverterErrorReporter.kt
@@ -2,13 +2,14 @@ package com.philkes.notallyx.data.model
 
 import android.app.Dialog
 import androidx.lifecycle.MutableLiveData
+import java.util.Collections
 import java.util.concurrent.atomic.AtomicBoolean
 
 /** Helper to register Converter errors while accessing the database. */
 object ConverterErrorReporter {
     var enabled = AtomicBoolean(true)
     val errors = MutableLiveData<Throwable?>(null)
-    val activeDialogs = mutableSetOf<Dialog>()
+    val activeDialogs = Collections.synchronizedSet(mutableSetOf<Dialog>())
 
     fun reportError(throwable: Throwable) {
         if (enabled.get() && errors.value == null) {
@@ -23,8 +24,8 @@ object ConverterErrorReporter {
     }
 
     fun dismissAllDialogs() {
-        activeDialogs.forEach { it.dismiss() }
-        activeDialogs.clear()
+        activeDialogs.toList().forEach { it.dismiss() }
+        synchronized(activeDialogs) { activeDialogs.clear() }
         errors.postValue(null)
     }
 }
