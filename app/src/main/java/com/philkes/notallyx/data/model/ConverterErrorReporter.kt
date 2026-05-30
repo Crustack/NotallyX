dffef4eb86983fc811343eabc8ee84ca574ebbf7
Crustack <crustackk@gmail.com>
2026-05-30 18:51:43 +0200

    Fix PR issues

--- app/src/main/java/com/philkes/notallyx/data/model/ConverterErrorReporter.kt
+++ app/src/main/java/com/philkes/notallyx/data/model/ConverterErrorReporter.kt
@@ -7,7 +7,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 /** Helper to register Converter errors while accessing the database. */
 object ConverterErrorReporter {
-    var enabled = AtomicBoolean(true)
+    val enabled = AtomicBoolean(true)
     val errors = MutableLiveData<Throwable?>(null)
     val activeDialogs = Collections.synchronizedSet(mutableSetOf<Dialog>())
 
